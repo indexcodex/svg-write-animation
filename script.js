50d26ec330be62d07f02cbe299c71aa16c08e7c1
indexcodex <60354991+indexcodex@users.noreply.github.com>
2020-04-07 19:59:39 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// this script will output the exact stroke-dasharray and stroke-dashoffset values
-const svgMotion = document.querySelectorAll('.svg-motion path');
-
-for (let i = 0; i < svgMotion.length; i++) {
-    console.log(`Path ${i + 1} is ${svgMotion[i].getTotalLength()}`);
-}
